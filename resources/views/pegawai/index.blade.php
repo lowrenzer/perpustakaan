7ed3cd82a48e0ed6c3c4d514694bcf6e0379570b
lowrenzer <rezapahlevi.25.rp@gmail.com>
2020-06-16 21:12:54 +0700

    tambah data

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -1,21 +1,37 @@
 @extends('layouts.app')
 @section('content')
-<table class="table table-striped">
-<tr>
+
+<div class="row">
+    <div class="col-md-10">
+        <div>
+            <a href="{{route ('pegawai.create')}}" class="btn-sm btn-danger">Tambah Pegawai</a>
+        </div>
+    <div class="card">
+    <div class="card-header">
+        Data Pegawai
+    </div>
+    <div class="card-body">
+    <table class="table table-striped">
+    <tr>
     <th>id pegawai</th>
     <th>Nama</th>
     <th>Gender</th>
     <th>No HP</th>
     <th>Alamat</th>
-</tr>
-@foreach ($datas as $data)
-<tr>
+    </tr>
+    @foreach ($datas as $data)
+    <tr>
     <th>{{$data->id_pegawai}}</th>
     <th>{{$data->nama}}</th>
     <th>{{$data->jenis_kelamin}}</th>
     <th>{{$data->no_hp}}</th>
     <th>{{$data->alamat}}</th>
-</tr>
-@endforeach
-</table>
+    </tr>
+    @endforeach
+    </table>
+    </div>
+</div>
+    </div>
+</div>
+
 @endsection
---
+++ resources/views/pegawai/tambah.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="row">
+    <div class="col-md-10">
+    <div class="card">
+    <div class="card-header">
+        Input Data Pegawai
+    </div>
+    <div class="card-body">
+    <form action="{{route('pegawai.store')}}" method="POST">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="id_pegawai">ID Pegawai</label>
+            <input type="text" name="id_pegawai" class="form-control" id="id_pegawai" placeholder="Id Pegawai">
+        </div>
+        <div class="form-group">
+            <label for="Nama">Nama Pegawai</label>
+            <input type="Text" name="nama" class="form-control" id="Nama" placeholder="Nama Pegawai">
+        </div>
+        <div class="form-group">
+            <label for="jenis_kelamin">Jenis Kelamin</label>
+            <select name="jenis_kelamin" id="" class="form-control">
+                <option value="">--Pilih--</option>
+                <option value="Pria">Pria</option>
+                <option value="Wanita">Wanita</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="no_hp">Nomer HP</label>
+            <input type="number" name="no_hp" class="form-control" id="no_hp" placeholder="Kontak Pegawai">
+        </div>
+        <div class="form-group">
+            <label for="Alamat">Alamat Pegawai</label>
+            <input type="Text" name="alamat" class="form-control" id="Alamat" placeholder="Alamat Pegawai">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Simpan</button>
+    </form>
+    </div>
+</div>
+    </div>
+</div>
+
+@endsection
